27fc0b2c5c42a214f612639243fb9566dfe1f8bc
Mahe1998 <87573259+Mahe1998@users.noreply.github.com>
2022-03-08 23:10:11 +0530

    Name Change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fname = "Mahesh";
+const fname = "Dahesh";
 const lname = "S";
 const currentDate = new Date();
 //console.log(currentDate);
