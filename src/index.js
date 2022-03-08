a8a0c265959d4ec70661ab4cd9724fcfec50f46a
Mahe1998 <87573259+Mahe1998@users.noreply.github.com>
2022-03-08 23:12:07 +0530

    Added Age

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const currentDate = new Date();
 ReactDOM.render(
   <div>
     <h1>Hello {fname + " " + lname}!</h1>
+    <h3>I am 24 years old.</h3>
     <p>The random number generated is {Math.round(Math.random() * 10)}.</p>
     <p>Created by {fname + " " + lname}.</p>
     <p>Copyright {currentDate.getFullYear()}</p>
