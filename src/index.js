a892de788331a6870feabc2c2299c53265a56966
Mahe1998 <87573259+Mahe1998@users.noreply.github.com>
2022-03-09 22:43:51 +0530

    Use of <img /> tag.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fname = "Mahesh";
-const lname = "S";
-const currentDate = new Date();
+// const fname = "Mahesh";
+// const lname = "S";
+// const currentDate = new Date();
 //console.log(currentDate);
 
 /* ReactDOM.render(
@@ -17,16 +17,55 @@ const currentDate = new Date();
   document.getElementById("root")
 ); */
 
-ReactDOM.render(
+/* ReactDOM.render(
   <div>
     <h1 className="heading">My Favorite Foods</h1>
     <ul>
       <li>prawn</li>
-      <li>Chickhen</li>
+      <li>Chicken</li>
       <li>Fish</li>
       <li>Egg</li>
       <li>Crab</li>
     </ul>
   </div>,
   document.getElementById("root")
+); */
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favorite Foods</h1>
+    <div>
+      <img
+        src={img + "?grayscale"}
+        alt="Random pics from Picsum in grayscale."
+      />
+      <img
+        className="food-img"
+        src="https://simmertoslimmer.com/wp-content/uploads/2018/01/South-Indian-chicken-biryani.jpg"
+        alt="Pic of Chicken Biryani"
+      />
+      <img
+        className="food-img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfJNQKpUo-2Q2iZstty0cI4sJiNDB_FBDQ6Q&usqp=CAU"
+        alt="Pic of Egg Biryani"
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
 );
+
+/* const img = "https://picsum.photos/200";
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favorite Foods</h1>
+    <div>
+      <img
+        src={img + "?grayscale"}
+        alt="Random pics from Picsum in grayscale."
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
+); */
